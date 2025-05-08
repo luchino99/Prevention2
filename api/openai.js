aba559a9fccb10f4d1d6226679bb3ec6ce0ba879
luchino99 <luca.imperio@live.it>
2025-05-08 14:46:20 +0200

    openai.js

--- api/openai.js
+++ api/openai.js
@@ -6,8 +6,9 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", "https://luchino99.github.io");
 
-  if (req.method === "OPTIONS") {
+ if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
