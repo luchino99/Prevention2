6e56563b4745be962ed62f9de53dd289a07ad47d
luchino99 <luca.imperio@live.it>
2025-07-30 15:03:16 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
 const supabaseUrl = 'https://nkkaxbmzacaxkwgtfmds.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
