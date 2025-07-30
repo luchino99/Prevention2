6f784885aa58b87cd2d47a1522ec00e705d82ad7
luchino99 <luca.imperio@live.it>
2025-07-30 14:53:41 +0200

    Update dashboard-logic.js

--- js/dashboard-logic.js
+++ js/dashboard-logic.js
@@ -1,6 +1,6 @@
 // Configuration constants
-const supabaseUrl = 'https://lwuhdgrkaoyvejmzfbtx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3dWhkZ3JrYW95dmVqbXpmYnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NzU1MDcsImV4cCI6MjA2MTI1MTUwN30.1c5iH4PYW-HeigfXkPSgnVK3t02Gv3krSeo7dDSqqsk';
+const supabaseUrl = 'https://nkkaxbmzacaxkwgtfmds.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Variabili globali per i dati utente
