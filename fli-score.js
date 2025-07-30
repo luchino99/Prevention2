22be784e0c11a5bf6fb8ebd204f3e45c80db22bf
luchino99 <luca.imperio@live.it>
2025-07-30 14:57:46 +0200

    Update fli-score.js

--- fli-score.js
+++ fli-score.js
@@ -1,10 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// ✅ Inizializza Supabase
-const supabase = createClient(
-  'https://lwuhdgrkaoyvejmzfbtx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3dWhkZ3JrYW95dmVqbXpmYnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NzU1MDcsImV4cCI6MjA2MTI1MTUwN30.1c5iH4PYW-HeigfXkPSgnVK3t02Gv3krSeo7dDSqqsk'
-);
+const supabaseUrl = 'https://nkkaxbmzacaxkwgtfmds.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 let listenerAttached = false;
 
